ec52ec39a6b318e9e90858977fa8c5805cc7ae7d
pat-hanbury <patrickhanbury1@gmail.com>
2019-02-09 14:07:37 -0500

    Added HTML file rendering compatability for homepage

--- swipazon/home/views.py
+++ swipazon/home/views.py
@@ -3,6 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def display_homepage(request):
+   return render(request, "homepage.html", {'today' : 'Friday'})
--- swipazon/swipazon/settings.py
+++ swipazon/swipazon/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+
+# Added stuff
+TEMPLATES_DIR = 'templates'
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -37,7 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 'Users.apps.UsersConfig',
+    # 'Users',
+    'home',
 ]
+
+# AUTH_USER_MODEL = 'Users.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
